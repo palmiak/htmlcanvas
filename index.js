762f5ce16b9cc0e30c091ff6cad9840e7b5a81d2
Maciej Palmowski <m.palmowski@freshpixels.pl>
2022-03-29 15:57:38 +0200

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const nodeHtmlToImage = require('node-html-to-image')
+
+nodeHtmlToImage({
+  output: './image.png',
+  html: `<html>
+    <head>
+      <style>
+        body {
+          background: red;
+          color: white;
+          font-size: 80px;
+        }
+      </style>
+    </head>
+    <body>Hello {{ name }}</body>
+  </html>
+  `,
+  content: { name: 'Maciek' }
+})
+  .then(() => console.log('The image was created successfully!'))
