63a39b442aace0c4aa83fa2f2e875a8c473e8fbc
Maciej Palmowski <m.palmowski@freshpixels.pl>
2022-03-30 01:34:59 +0200

    Beta - posts

--- index.js
+++ index.js
@@ -1,24 +1,82 @@
+require('dotenv').config()
+
 const nodeHtmlToImage = require('node-html-to-image');
 const { hideBin } = require('yargs/helpers')
 const yargs = require('yargs');
-const argv = yargs(hideBin(process.argv)).argv
+const argv = yargs(hideBin(process.argv)).argv;
 
-nodeHtmlToImage({
-  output: './image.png',
-  html: `<html>
-    <head>
-      <style>
-        body {
-          background: red;
-          color: white;
-          font-size: 80px;
-        }
-      </style>
-    </head>
-    <body>Hello {{ name }}</body>
-  </html>
-  `,
-  content: { name: argv.name },
-  puppeteerArgs: { args: ['--no-sandbox'] }
-})
-  .then(() => console.log('The image was created successfully!'))
+const WPAPI = require( 'wpapi' );
+let wp = new WPAPI(
+   {
+     endpoint: process.env.API_URL,
+     username: process.env.WP_USERNAME,
+     password: process.env.WP_PASSWORD
+   }
+);
+
+wp.posts().id( argv.post_id ).then( ( post ) => {
+  title = post.title.rendered;
+
+  nodeHtmlToImage({
+    output: './images/image.png',
+    html: `<html>
+      <head>
+        <style>
+          body {
+            background: #1a67fd;
+            text-align:center;
+            display: grid;
+            justify-content: center;
+            align-content: center;
+            font-family: sans-serif;
+            width:1200px;
+            height:600px;
+          }
+
+          h1 {
+            color: white;
+            font-size: 80px;
+          }
+
+          p {
+            color: white;
+            font-size: 30px;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>{{ title }}</h1>
+        <p>New post<p>
+      </body>
+    </html>
+    `,
+    content: {
+      title: title,
+    },
+    puppeteerArgs: { args: ['--no-sandbox'] }
+  })
+    .then(
+      () => {
+        const responseUploadImage = wp.media()
+        .file( './images/image.png' )
+        .create({
+            title: 'My awesome image',
+            alt_text: 'an image of something awesome',
+            caption: 'This is the caption text',
+            description: 'More explanatory information'
+        }).then( ( responseUploadImage ) => {
+          wp.posts().id( argv.post_id ).update({
+            featured_media: responseUploadImage.id
+          });
+        })
+        .catch( function( responseUploadImage ) {
+          console.log( responseUploadImage );
+        } ) ;
+      }
+  )
+}).catch( (error) => {
+  console.log( error );
+});
+
+
+
